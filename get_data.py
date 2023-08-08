0835104597372545b2bca0b4045014fb5034ecb3
yandi <yandi888@icloud.com>
2023-08-08 23:58:46 +0800

    更新数据，使用MySQL作为数据储存

--- get_data.py
+++ get_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import yaml
+from sqlalchemy import create_engine
 
 # 读取配置文件
 with open("config.yaml", "r") as file:
@@ -19,13 +20,21 @@ def get_data(gen=1):
         pass
     elif gen == 2:
         # 从MySQL数据库中读取调查表和防治表
-        df_survey = pd.read_sql_table('2023年第二代美国白蛾调查表',
-                                      'mysql+pymysql://{}:{}@{}:{}/{}'.format(username, password, host, port, database))
-        df_treatment = pd.read_sql_table('2023年第二代美国白蛾防治表',
-                                         'mysql+pymysql://{}:{}@{}:{}/{}'.format(username, password, host, port,
-                                                                                 database))
+        # 建立数据库连接
+        engine = create_engine('mysql+pymysql://{}:{}@{}:{}/{}'.format(username, password, host, port, database))
+        # 执行SQL查询并获取数据
+        query_survey = "SELECT * FROM `2023年第二代美国白蛾调查表`"
+        query_treatment = "SELECT * FROM `2023年第二代美国白蛾防治表`"
+        df_survey = pd.read_sql(query_survey, engine)
+        df_survey['调查日期'] = pd.to_datetime(df_survey['调查日期'], format='%Y-%m-%d')
+        df_survey['派单时间'] = pd.to_datetime(df_survey['派单时间'], format='%Y-%m-%d')
+        df_treatment = pd.read_sql(query_treatment, engine)
+        df_treatment['防治日期'] = pd.to_datetime(df_treatment['防治日期'], format='%Y-%m-%d')
+        # 关闭数据库连接
+        engine.dispose()
     elif gen == 3:
         pass
+    print(df_survey.info())
     # 获取每个点位第一次受害的数据
     df_survey_filtered = df_survey[df_survey['受害株数'] > 0]
     df_survey_filtered_grouped = df_survey_filtered.groupby('点位编号')
