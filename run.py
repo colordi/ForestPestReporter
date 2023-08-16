cc1a4721577815136b881b0f7826cae53c0a35ea
yandi <yandi888@icloud.com>
2023-08-16 16:45:08 +0800

    清空项目

--- run.py
+++
@@ -1,6 +0,0 @@
-import streamlit
-import streamlit.web.bootstrap as bootstrap
-
-if __name__ == '__main__':
-    streamlit._is_running_with_streamlit = True
-    bootstrap.run('主页.py', 'streamlit run', (), {})
