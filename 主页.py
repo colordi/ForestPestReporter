2502f0df356e5a47f07ad524cd0d9450eb4bc721
yandi <yandi888@icloud.com>
2023-09-13 09:15:39 +0800

    更新了2023-09-12的数据。

--- 主页.py
+++ 主页.py
@@ -1,5 +1,15 @@
 import streamlit as st
 
+update = """
+# 更新记录
+- 2023-09-12的数据已上传，且更新防治记录；
+- 2023-09-11的数据已上传；
+- 2023-09-10的数据已上传；
+- 2023-09-08的数据已上传；
+- 2023-09-07的数据已上传；
+- 2023-09-06的数据已上传；
+"""
+
 
 # 调整页面布局
 st.set_page_config(layout='wide')
@@ -7,3 +17,4 @@ st.set_page_config(layout='wide')
 if __name__ == '__main__':
     # 设置页面标题
     st.title("林业有害生物防治系统")
+    st.markdown(update)
